f4f58681910f816f87402fa95987aa1803f2c338
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-01-20 21:32:19 +0530

    Add a function to play a round

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function getComputerChoice() {
-    let ran = Math.random();
-    if( ran < 0.33) return "rock";
-    else if( ran < 0.66 ) return "paper";
+    const ran = Math.random();
+    if (ran < 0.33) return "rock";
+    else if (ran < 0.66) return "paper";
     else return "scissor";
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return playRound(prompt("Tie choose again!"), getComputerChoice());
+    } else if (
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissor") ||
+        (playerSelection === "scissor" && computerSelection === "rock")
+    ) {
+        return "You lost this round";
+    } else {
+        return "You won this round";
+    }
 }
