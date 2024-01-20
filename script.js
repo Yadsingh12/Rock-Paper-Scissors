40de2c73bc3d6d915e92576c2257b8a5411fe3ef
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-01-20 21:36:01 +0530

    Add a function to make game multi rounded

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ function playRound(playerSelection, computerSelection) {
         return "You won this round";
     }
 }
+
+
+function game() {
+    console.log("Let's play a rock, paper and scissors game!!!");
+    console.log("The game's winner will be decided by best of 5");
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i=0; i<5; i++) {
+        const roundResult = playRound(prompt("Choose your choice:"), getComputerChoice());
+        console.log(roundResult);
+        if(roundResult === "You lost this round kiddo") computerScore++;
+        else playerScore++;
+        console.log(`player's score: ${playerScore} computer's score: ${computerScore}`);
+    }
+    if (playerScore > computerScore) return "You won man! congo";
+    else return "You lost kiddo";
+}
