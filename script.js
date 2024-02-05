1447255032f7b1392205212c3f24b7383b293dba
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-05 22:26:59 +0530

    Add a div to output result of a round

--- script.js
+++ script.js
@@ -6,38 +6,25 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    const result = document.querySelector("#result");
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        console.log("Tie:choose again");
+        result.textContent="Tie:choose again";
     } else if (
         (playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissor") ||
         (playerSelection === "scissor" && computerSelection === "rock")
     ) {
-        console.log("You lost this round");
+        result.textContent="You lost this round";
     } else {
-        console.log("You won this round");
+        result.textContent= "You won this round";
     }
 }
 
-function game() {
-    console.log("Let's play a rock, paper and scissors game!!!");
-    console.log("The game's winner will be decided by best of 5");
-    let playerScore = 0;
-    let computerScore = 0;
-    const roundResult = playRound(prompt("Choose your choice:"), getComputerChoice());
-    console.log(roundResult);
-    if (roundResult === "You lost this round") computerScore++;
-    else playerScore++;
-    console.log(`player's score: ${playerScore} computer's score: ${computerScore}`);
-    //if (playerScore > computerScore) return "You won man! congo";
-    //else return "You lost kiddo";
-}
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissor = document.querySelector("#scissor");
 rock.addEventListener("click",()=>(playRound("rock",getComputerChoice())));
 paper.addEventListener("click",()=>(playRound("paper",getComputerChoice())));
 scissor.addEventListener("click",()=>(playRound("scissor",getComputerChoice())));
-//console.log(game());
