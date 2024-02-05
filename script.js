39f37d4b90fb5996dc0ef0c1a25949b3f1354d69
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-05 23:01:02 +0530

    Made the game to be of 5 rounds.

--- script.js
+++ script.js
@@ -6,25 +6,54 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    const result = document.querySelector("#result");
+    const roundResult = document.querySelector("#round-result");
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        result.textContent="Tie:choose again";
+        roundResult.textContent="Tie:choose again";
     } else if (
         (playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissor") ||
         (playerSelection === "scissor" && computerSelection === "rock")
     ) {
-        result.textContent="You lost this round";
+        roundResult.textContent="You lost this round";
+        i++;
+        computerScore++;
     } else {
-        result.textContent= "You won this round";
+        roundResult.textContent= "You won this round";
+        i++;
+        userScore++;
+    }
+    result.textContent = `User score: ${userScore}  Computer score: ${computerScore}`;
+    if(i>=5) {
+        if(userScore>computerScore){
+            result.textContent = "You won";
+        } else {
+            result.textContent = "You lost";
+        }
+        rock.remove();
+        paper.remove();
+        scissor.remove();
+        roundResult.remove();
     }
 }
+
+let i=0;
+let userScore=0;
+let computerScore=0;
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissor = document.querySelector("#scissor");
-rock.addEventListener("click",()=>(playRound("rock",getComputerChoice())));
-paper.addEventListener("click",()=>(playRound("paper",getComputerChoice())));
-scissor.addEventListener("click",()=>(playRound("scissor",getComputerChoice())));
+
+const result = document.querySelector("#result");
+
+rock.addEventListener("click",()=> {
+        playRound("rock",getComputerChoice());
+});
+paper.addEventListener("click",()=> {
+    playRound("paper",getComputerChoice());
+});
+scissor.addEventListener("click",()=> {
+    playRound("scissor",getComputerChoice());
+});
